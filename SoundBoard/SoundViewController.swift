29fc7ec5d1b7a6776d61897ebe3b43a9464c26e1
A.Fairiz <afairiz@gmail.com>
2016-11-27 14:01:36 +0800

    App Complete

--- SoundBoard/SoundViewController.swift
+++ SoundBoard/SoundViewController.swift
@@ -7,29 +7,99 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
+    var audioURL: URL?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       setupRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupRecorder(){
+        do{
+            // Create audio session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // Create URL for audio file
+            let basePath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
+            // Create settings
+            var settings: [String : Any] = [:]
+            settings[AVFormatIDKey] = kAudioFormatMPEG4AAC
+            settings[AVSampleRateKey] = 44100
+            settings[AVNumberOfChannelsKey] = 2
+            
+            // Create AudioRecorder object
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        }catch let error as NSError{
+            print(error)
+        }
+        
     }
-    */
+    
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        
+        if audioRecorder!.isRecording{
+            // Stop the recording
+            audioRecorder?.stop()
+            
+            // Change button title to Record
+            recordButton.setTitle("Record", for: .normal)
+            
+            // Enable Play and Add Buttons
+            playButton.isEnabled = true
+            addButton.isEnabled = true
+            
+        }else{
+            // Start the recording
+            audioRecorder?.record()
+            
+            // Change button title to Stop
+            recordButton.setTitle("Stop", for: .normal)
+            
+        }
+    }
 
+    @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let sound = Sound(context: context)
+        
+        sound.name = nameTextField.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: audioURL!)
+            audioPlayer?.play()
+        }catch{
+            print(error)
+        }
+    }
 }
--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sounds: [Sound] = []
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            sounds = try context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        }catch{}
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let sound = sounds[indexPath.row]
+        
+        cell.textLabel?.text = sound.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sound = sounds[indexPath.row]
+        do{
+            audioPlayer = try AVAudioPlayer(data: sound.audio as! Data)
+            audioPlayer?.play()
+        }catch{}
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let sound = sounds[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(sound)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                sounds = try context.fetch(Sound.fetchRequest())
+                tableView.reloadData()
+            }catch{}
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
 
 }
 
